4c3da7d53992dbfa2ad9e97ead21efa041badcfa
narcisolobo <narcisolobo@gmail.com>
2023-11-07 19:54:07 -0800

    added javascript code

---
+++ 04-variables-and-datatypes/index.js
@@ -0,0 +1,22 @@
+// Variable Declaration
+var numberOfToys;
+
+// Assigning a value to a variable
+numberOfToys = 5; // number datatype
+
+// Rules of JavaScript variable names
+/* 
+  1. No spaces
+  2. Cannot start with a number
+  3. Cannot use reserved keywords
+*/
+
+// Variable declaration and initialization
+var myName = 'Narciso Lobo'; // string datatype
+
+var isInstructor = true; // boolean datatype
+
+// Three Primitive Datatypes
+// Numbers - Integers or decimal numbers
+// Strings - Sequences of characters
+// Booleans - True or false
